a6f0befc5855710b35a9457bdb02263388d9c6ad
Kadda Aoues <houes_k@yahoo.fr>
2024-12-03 17:45:57 +0300

    new name

---
+++ exemples/main.cpp
@@ -0,0 +1,20 @@
+//				this file for execution game() function 
+
+#include <iostream>
+#include <project/RandomBox/random_box.hpp>
+
+
+int main() {
+
+	std::cout << "----------------------------------------------------------\n";
+	std::cout << "- Game Start !\n";
+	std::cout << "----------------------------------------------------------\n";
+
+	// Sun Dec  1 22:52:16 2024
+	game(145, 35, 8, 14);
+
+
+	std::cout << "----------------- Game Over -------------------------\n";
+
+	return 0;
+}
