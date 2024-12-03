1d364be1d676a5f9b277b6ae6efc2b7a584705d7
Kadda Aoues <houes_k@yahoo.fr>
2024-12-03 17:53:50 +0300

    histogram project

---
+++ project/Histogram/ka_Axes.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <console project/include/component/ka_SString.hpp>
+
+
+
+namespace histo {
+
+
+	class AxeX {
+
+	};
+
+
+	class AxeY {
+
+
+	};
+
+
+}
---
+++ project/Histogram/ka_histogram.hpp
@@ -0,0 +1,34 @@
+/*
+					Histogram Project 
+					Author : Kadda Aoues
+					Date	: 27 / 11 /2024
+					Version	: 1.0.0
+
+*/
+#pragma once
+
+
+
+
+namespace histo {
+
+	class Data {
+
+	};
+
+
+	class AxeX {
+
+
+	};
+
+
+	class AxeY {
+
+
+	};
+
+
+
+
+} // End of name space histo
