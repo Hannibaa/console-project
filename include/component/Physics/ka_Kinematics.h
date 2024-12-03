9173c9096a7d5b0eb2aa174948b5061b9ffc9317
Kadda Aoues <houes_k@yahoo.fr>
2024-12-03 17:50:16 +0300

    move defined class to another header files

--- include/component/Physics/ka_Kinematics.h
+++ include/component/Physics/ka_Kinematics.h
@@ -12,85 +12,6 @@ namespace physic {
 	using MoveInterface1	=  I1Move<fVec2, fRect>;
 	using MoveInterface2	=  I0Move<fVec2, fRect>;
 
-	// ------------------------------------- //
-	// Free Move							 //
-	// ------------------------------------- //
-	class FreeMove1 : public MoveInterface1
-	{
-		float vx = 1.f;
-		float vy = 1.f;
-
-	public:
-
-		void move(console::ITransformable<float>* pM, float Dt) override
-		{
-			auto p = pM->get_position();
-			auto d = pM->get_dimension();
-
-			p.x += _velocity.x * Dt * vx;
-			p.y += _velocity.y * Dt * vy;
-
-			if (p.x < _space.x || p.x > _space.x + _space.dx - d.x + 2.f) vx *= -1.f;
-			if (p.y < _space.y || p.y > _space.y + _space.dy - d.y + 2.f) vy *= -1.f;
-
-			math::clamp(p.x, _space.x, _space.x + _space.dx);
-			math::clamp(p.y, _space.y, _space.y + _space.dy);
-
-			pM->set_position(p);
-
-		}
-	};
-
-	template<typename T>
-	class FreeMove2 : public MoveInterface2 {
-
-		float vx = 1.f;
-		float vy = 1.f;
-
-	public:
-
-		using value_type = T;
-
-		void move(void* ptr, float Dt) override
-		{
-			auto p = static_cast<value_type*>(ptr)->get_position();
-			auto d = static_cast<value_type*>(ptr)->get_dimension();
-
-			p.x += _velocity.x * Dt * vx;
-			p.y += _velocity.y * Dt * vy;
-
-			if (p.x < _space.x || p.x > _space.x + _space.dx - d.x + 2.f) vx *= -1.f;
-			if (p.y < _space.y || p.y > _space.y + _space.dy - d.y + 2.f) vy *= -1.f;
-
-			static_cast<value_type*>(ptr)->set_position(p);
-		}
-	};
-
-	template<typename T>
-	class FreeMove0 : public MoveInterface0
-	{
-
-		float vx = 1.f;
-		float vy = 1.f;
-
-	public:
-
-		using value_type = T;
-
-		void move(void* ptr, float Dt) override
-		{
-			auto p = static_cast<value_type*>(ptr)->get_position();
-			auto d = static_cast<value_type*>(ptr)->get_dimension();
-
-			p.x += _velocity.x * Dt * vx;
-			p.y += _velocity.y * Dt * vy;
-
-			if (p.x < _space.x || p.x > _space.x + _space.dx - d.x + 2.f) vx *= -1.f;
-			if (p.y < _space.y || p.y > _space.y + _space.dy - d.y + 2.f) vy *= -1.f;
-
-			static_cast<value_type*>(ptr)->set_position(p);
-		}
-	};
 
 	// function ...................................................................................
 	template<typename Movable, typename Vector, typename Space>
@@ -112,10 +33,17 @@ namespace physic {
 	}
 
 
-	//////////////////////////////////////////////////////////////////////////////////////////////////////////
-	// 
-	//     Controled Move    
-	// 
-	//////////////////////////////////////////////////////////////////////////////////////////////////////////
+	/*template<typename Movable>
+	void moveTo(Movable* ptr, const fVec2& B, float Dt)
+	{
+		static float _t = 0;
+		auto A = ptr->get_position();
+		fVec2 p{};
+		_t += 
+		p.x += _velocity.x * Dt * vx;
+		p.y += _velocity.y * Dt * vy;
+
+		ptr->set_position(p);
+	}*/
 
 }
